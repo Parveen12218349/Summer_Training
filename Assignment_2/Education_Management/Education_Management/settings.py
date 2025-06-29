54542611e51cf4fa5bb64470db951cfb0f943e83
Parveen <pk01052003@gmail.com>
2025-06-29 15:33:26 +0530

    update

--- Assignment_2/Education_Management/Education_Management/settings.py
+++ Assignment_2/Education_Management/Education_Management/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'course_mgnt',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Education_Management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'education',
+        'USER': 'postgres',
+        'PASSWORD': '2198420Pk',
+        'host': 'localhost',
+        'PORT': '5432',
     }
 }
 
